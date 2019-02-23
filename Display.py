8af373f66844a510201779166d5e8ddf19614cb2
Rohan Barman <rohan1barman@gmail.com>
2019-02-22 23:46:25 -0600

    cleaned up imports and added docstrings to functions in Display.py

--- Display.py
+++ Display.py
@@ -1,26 +1,31 @@
 import streamlink
 import cv2
 
-import numpy as np
-import cv2 as cv
-
 '''Methods related to displaying video via opencv'''
 
+def display_stream(url):
+	'''Display an opencv VideoCapture from a streamlink supported stream'''
+	stream = get_stream(url)
+	cap = cv2.VideoCapture(stream)
+	display_capture(cap)
+
 def get_stream(url, quality='best'):
+	'''Get streamlink supported stream from url'''
 	streams = streamlink.streams(url)
 	if streams:
 		return streams[quality].to_url()
 	else:
 		raise ValueError("No steams were available")
 
-def display_stream(url):
-	stream = get_stream(url)
-	cap = cv2.VideoCapture(stream)
+def display_webcam():
+	'''Display an opecv VideoCapture from webcam'''
+	cap = cv2.VideoCapture(0)
 	display_capture(cap)
 
 def display_capture(cap):
-
-	face_cascade = cv.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+	'''Display an opencv VideoCapture with face detection'''
+	
+	face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 	frame_time = int((1.0 / 30.0) * 1000.0)
 	
 	while True:
@@ -30,10 +35,10 @@ def display_capture(cap):
 				'''detect face on frame
 					TODO: fix slow performance on streaming video
 				'''
-				gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+				gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 				faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 				for (x,y,w,h) in faces:
-				    cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),5)
+				    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),5)
 
 				cv2.imshow('frame', frame)
 				if cv2.waitKey(frame_time) & 0xFF == ord('q'):
@@ -45,7 +50,3 @@ def display_capture(cap):
 
 	cv2.destroyAllWindows()
 	cap.release()
-
-def display_webcam():
-	cap = cv2.VideoCapture(0)
-	display_capture(cap)
